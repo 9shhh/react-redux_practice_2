fb5ea9c824e857ae9eab97a467f54711cfd3b2fd
seonghwanPark <psh@esellers.co.kr>
2020-05-19 10:43:35 +0900

    upload

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,3 +1,5 @@
+import { createAction, handleActions } from 'redux-actions';
+
 /* 
     1. 액션 타입 정의 
      - 액션 타입은 대문자로 정의
@@ -11,8 +13,12 @@ const DECREASE = 'counter/DECREASE';
     2. 액션 생성 함수 만들기 (export를 사용하여 다른 파일에서 불러오게 하기)
 */
 
-export const increase = () => ({type : INCREASE});
-export const decrease = () => ({type : DECREASE});
+// export const increase = () => ({type : INCREASE});
+// export const decrease = () => ({type : DECREASE});
+
+// 위에 수동으로 만들었던 액션 생성 함수를 redux-actions 모듈을 사용하여 편리하게 생성
+export const increase = createAction(INCREASE);
+export const increase = createAction(DECREASE);
 
 /*
     3. 초기 상태 및 리듀서 함수 만들기
@@ -22,20 +28,29 @@ const initialState = {
     number: 0
 };
 
-function counter(state = initialState, action) {
-    switch (action.type) {
-        case INCREASE: 
-            return {
-                number: state.number + 1
-            };
-        case DECREASE:
-            return {
-                number: state.number -1
-            };
-        default: 
-            return state;    
-    }
-}
+// function counter(state = initialState, action) {
+//     switch (action.type) {
+//         case INCREASE: 
+//             return {
+//                 number: state.number + 1
+//             };
+//         case DECREASE:
+//             return {
+//                 number: state.number -1
+//             };
+//         default: 
+//             return state;    
+//     }
+// }
+
+// redux-actions 모듈의 handleActions를 사용하여 리듀서 편리하게 생성 (첫번째 파라미터에는 각 액션에 대한 업데이트 함수, 두번째 파라미터에는 초기 상태)
+const counter = handleActions(
+    {
+        [INCREASE]: (state, action) => ({ number : state.number + 1 }),
+        [DECREASE]: (state, action) => ({ number : state.number - 1 }),
+    },
+    initialState,
+);
 
 export default counter;
 
--- src/modules/todos.js
+++ src/modules/todos.js
@@ -2,6 +2,8 @@
     1. 액션 타입 정의
  */
 
+import { createAction, handleAction } from "redux-actions";
+
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // input 값을 변경함
 const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
 const TOGGLE = 'todos/TOGGLE'; // todo를 체크 or 체크 해제 함
@@ -11,30 +13,47 @@ const REMOVE = 'todos/REMOVE'; // todo를 제거함
     2. 액션 생성 함수 만들기
  */
 
-export const changeInput = (input) => ({
-    type: CHANGE_INPUT,
-    input,
-});
+// export const changeInput = (input) => ({
+//     type: CHANGE_INPUT,
+//     input,
+// });
+
+// let id = 3; // insert가 호출될 때마다 1씩 더해짐.
+// export const insert = (text) => ({
+//     type: INSERT,
+//     todo: {
+//         id: id++,
+//         text,
+//         done: false,
+//     },
+// });
+
+// export const toggle = (id) => ({
+//     type: TOGGLE,
+//     id,
+// });
+
+// export const remove = (id) => ({
+//     type: REMOVE,
+//     id,
+// });
+
+/* 
+   createAction으로 액션을 만들면 액션에 필요한 추가 데이터는 payload라는 이름을 사용, 추가로 payload의 값을 변형을 주어서 넣고 싶다면 
+   createAction의 두번째 파라미터로 payload를 정의하는 함수를 따로 선언해서 넣어 주면됨. 
+*/
+
+export const changeInput = createAction(CHANGE_INPUT, input => input);
 
 let id = 3; // insert가 호출될 때마다 1씩 더해짐.
-export const insert = (text) => ({
-    type: INSERT,
-    todo: {
-        id: id++,
-        text,
-        done: false,
-    },
-});
+export const insert = createAction(INSERT, text => ({
+    id: id++,
+    text,
+    done: false,
+}));
 
-export const toggle = (id) => ({
-    type: TOGGLE,
-    id,
-});
-
-export const remove = (id) => ({
-    type: REMOVE,
-    id,
-});
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
 
 /*
     3. 초기 상태 및 리듀서 함수 만들기
@@ -58,33 +77,43 @@ const initialState = {
     ],
 };
 
-function todos(state = initialState, action) {
-    switch(action.type) {
-        case CHANGE_INPUT: 
-            return {
-                ...state,
-                input: action.input
-            };
-        case INSERT: 
-            return {
-                ...state,
-                todos: state.todos.concat(action.todo)
-            };
-        case TOGGLE: 
-            return {
-                ...state,
-                todos: state.todos.map(todo => todo.id === action.id ? { ... todo, done: !todo.done } : todo)
-            };
-        case REMOVE: 
-            return {
-                ...state,
-                todos: state.todos.filter(todo => todo.id !== action.id)
-            };
+// function todos(state = initialState, action) {
+//     switch(action.type) {
+//         case CHANGE_INPUT: 
+//             return {
+//                 ...state,
+//                 input: action.input
+//             };
+//         case INSERT: 
+//             return {
+//                 ...state,
+//                 todos: state.todos.concat(action.todo)
+//             };
+//         case TOGGLE: 
+//             return {
+//                 ...state,
+//                 todos: state.todos.map(todo => todo.id === action.id ? { ... todo, done: !todo.done } : todo)
+//             };
+//         case REMOVE: 
+//             return {
+//                 ...state,
+//                 todos: state.todos.filter(todo => todo.id !== action.id)
+//             };
             
-        default:
-            return state;
-    }
-}
+//         default:
+//             return state;
+//     }
+// }
+
+const todos = handleActions(
+    {
+        [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+        [INSERT]: (state, { payload: todo}) => ({ ...state, todos: state.todos.concat(todo)}),
+        [TOGGLE]: (state, { payload: id }) => ({ ...state, todos: state.todos.map(todo => todo.id === id ? { ...todo, done: !todo.done } : todo)}),
+        [REMOVE]: (state, { payload: id} ) => ({ ...state, todos: state.todos.filter(todo => todo.id !== actiod)}),
+    },
+    initialState
+);
 
 export default todos;
 
